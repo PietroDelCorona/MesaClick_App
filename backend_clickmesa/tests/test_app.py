7c88dc06bebc99ca97fb8abd143f9ff7e45a07db
Pietro Del Corona <pietrogdelcorona@gmail.com>
2025-04-22 20:50:34 -0300

    initial project config

---
+++ backend_clickmesa/tests/test_app.py
@@ -0,0 +1,15 @@
+
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from backend_clickmesa.app import app
+
+
+def test_root_must_return_ok_and_hello_world():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Hello World!'}
